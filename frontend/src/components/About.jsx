baaebabeb3db069ff8f00b8109f33cac349edaa8
Rajeev R Sharma <i.rarsh@gmail.com>
2023-01-03 22:22:18 +0530

    Chore: Added visuals for How to play on about page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,3 +1,8 @@
+import begin from '../assets/media/images/begin.png';
+import end from '../assets/media/images/end.png';
+import intermediate1 from '../assets/media/images/intermediate-1.png';
+import intermediate2 from '../assets/media/images/intermediate-2.png';
+import intermediate3 from '../assets/media/images/intermediate-3.png';
 import './About.css';
 
 export const About = () => {
@@ -5,25 +10,62 @@ export const About = () => {
     <div className='about-container'>
       <div className='about-section'>
         <div className='title'>How to play?</div>
-        <p>
-          <strong>Start with the green coin</strong>, and{' '}
-          <strong>reach the red coin</strong> while collecting the maximum gold.
-          There are many paths possible, choose one which gives you maximum
-          gold.
-        </p>
-        <p>
-          You can only go <strong>up / down</strong>, or{' '}
-          <strong>left / right</strong>. Also, <strong>red dashed lines</strong>{' '}
-          represent <strong>walls</strong> that you can't cross.
-        </p>
-        <p>
-          <strong>Play as many times as you want.</strong> You win, if you get
-          as much gold as is displayed at the start of the game.
-        </p>
-        <p>
-          <strong>Try treading a new road everyday.</strong> The game refreshes
-          everyday at <strong>12:00 AM GMT</strong>.
-        </p>
+        <figure>
+          <figcaption>
+            1. Start by <strong>tapping the green coin.</strong>
+          </figcaption>
+          <img src={begin} alt='starting coin' width={80} />
+        </figure>
+        <figure>
+          <figcaption>
+            2. Your goal is to <strong>reach the red coin</strong> by collecting
+            the maximum possible coins (running counter shown above the game).
+          </figcaption>
+          <img src={end} alt='final coin' width={40} />
+        </figure>
+        <figure>
+          <figcaption>
+            3. Every coin tap makes the neighboring coins (in up-down &
+            left-right directions) green, i.e. tappable.
+          </figcaption>
+          <img src={intermediate1} alt='intermediate game board' width={120} />
+        </figure>
+        <figure>
+          <figcaption>
+            4. But if there is a <strong>wall</strong> in between (
+            <strong>red dashed lines</strong>) then you won't be able to go in
+            that direction.
+          </figcaption>
+          <img
+            src={intermediate2}
+            alt='alternative intermediate game board'
+            width={120}
+          />
+        </figure>
+        <figure>
+          <figcaption>
+            5. Some paths may lead to dead ends, or lesser number of coins.{' '}
+            <strong>
+              To win, get to the red coin with the needed number of coins.
+            </strong>
+          </figcaption>
+          <img
+            src={intermediate3}
+            alt='intermediate game board with path'
+            width={120}
+          />
+        </figure>
+        <figure>
+          <figcaption>
+            6. <strong>Play as many times as you want.</strong>
+          </figcaption>
+        </figure>
+        <figure>
+          <figcaption>
+            7. <strong>Comeback tomorrow for a new puzzle.</strong> The game
+            refreshes everyday at <strong>12:00 AM GMT</strong>
+          </figcaption>
+        </figure>
       </div>
       <div className='about-section'>
         <div className='title'>About</div>
@@ -34,6 +76,17 @@ export const About = () => {
         </p>
       </div>
       <div className='about-footer'>
+        <div style={{ marginBottom: '1rem' }}>
+          Follow GoldRoad on Twitter{' '}
+          <a
+            href='https://twitter.com/thegoldroad'
+            target='_blank'
+            rel='noreferrer'
+          >
+            @TheGoldRoad
+          </a>
+        </div>
+
         <div>
           Made with ❤️ & coffee, by{' '}
           <a
@@ -45,6 +98,7 @@ export const About = () => {
           </a>
         </div>
         <div>
+          For feedback, or anything else{' '}
           <a href='mailto:i.rarsh@gmail.com?subject=[GoldRoad]'>Contact me</a>
         </div>
       </div>
