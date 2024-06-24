853322921f6c37f54408136cc928570f984131b9
Rajeev R Sharma <i.rarsh@gmail.com>
2024-06-24 21:59:19 +0530

    feat: return the correct type from data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,7 +1,8 @@
 import { gameData } from '@/app/lib/dummy-data';
+import { Game } from '@/app/lib/types';
 
 export async function fetchGame() {
-  const game = await new Promise((resolve) => {
+  const game = await new Promise<Game>((resolve) => {
     setTimeout(() => {
       resolve(gameData);
     }, 2500);
