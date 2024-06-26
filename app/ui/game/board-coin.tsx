9bd137f7dd50802b30e83e2b72b09d9c15d75d3e
Rajeev R Sharma <i.rarsh@gmail.com>
2024-06-26 22:03:43 +0530

    feat: make coin component changes

--- app/ui/game/board-coin.tsx
+++ app/ui/game/board-coin.tsx
@@ -2,18 +2,31 @@ import clsx from 'clsx';
 import styles from '@/app/ui/game/board-coin.module.css';
 import { Coin, CoinWall } from '@/app/lib/types';
 
-export default function BoardCoin({ coin }: { coin: Coin }) {
+export default function BoardCoin({
+  coin,
+  onClick,
+}: {
+  coin: Coin;
+  onClick: (index: number) => void;
+}) {
   return (
-    <div
+    <button
       className={clsx(styles.coin, {
-        [styles['wall-left']]: coin.wall === CoinWall.LEFT,
-        [styles['wall-eight']]: coin.wall === CoinWall.RIGHT,
-        [styles['wall-top']]: coin.wall === CoinWall.TOP,
-        [styles['wall-bottom']]: coin.wall === CoinWall.BOTTOM,
+        [styles['wall-left']]: coin.wall === CoinWall.Left,
+        [styles['wall-right']]: coin.wall === CoinWall.Right,
+        [styles['wall-top']]: coin.wall === CoinWall.Top,
+        [styles['wall-bottom']]: coin.wall === CoinWall.Bottom,
+        [styles['coin--end']]: coin.isEnd,
+        [styles['coin--active']]: coin.state === 'active',
+        [styles['coin--done']]: coin.state === 'done',
       })}
+      type='button'
+      role='gridcell'
+      tabIndex={coin.tabIndex}
+      onClick={() => onClick(coin.index)}
     >
       <div className={styles['coin-border']} />
       {coin.value}
-    </div>
+    </button>
   );
 }
