4e159a636845c9ac15b76f04e629ac3ae076f869
Rajeev R Sharma <i.rarsh@gmail.com>
2024-06-29 16:27:13 +0530

    inprogress: game play changes

--- app/ui/game/game-board.tsx
+++ app/ui/game/game-board.tsx
@@ -1,10 +1,87 @@
 'use client';
 
-import { useState } from 'react';
+import { MouseEventHandler, useCallback, useState } from 'react';
 import SoundManager from '@/app/lib/sound-manager';
 import { Coin, CoinState, CoinWall, Game } from '@/app/lib/types';
 import BoardCoin from '@/app/ui/game/board-coin';
 import styles from '@/app/ui/game/game-board.module.css';
+
+const changeCoinState = (
+  coin: Coin | null,
+  newCoins: Coin[],
+  state: CoinState
+) => {
+  if (coin && coin.state !== 'done') {
+    newCoins[coin.index] = { ...coin, state };
+    return true;
+  }
+
+  return false;
+};
+
+const handleNeighboringCoins = (
+  currIndex: number,
+  newCoins: Coin[],
+  makeActive: boolean,
+  gameCols: number
+) => {
+  const topCoin =
+    currIndex - gameCols >= 0 ? newCoins[currIndex - gameCols] : null;
+  const bottomCoin =
+    currIndex + gameCols < newCoins.length
+      ? newCoins[currIndex + gameCols]
+      : null;
+  const leftCoin =
+    currIndex % gameCols && currIndex - 1 >= 0 ? newCoins[currIndex - 1] : null;
+  const rightCoin =
+    (currIndex + 1) % gameCols !== 0 && currIndex + 1 < newCoins.length
+      ? newCoins[currIndex + 1]
+      : null;
+
+  let totalChanges = 0;
+
+  if (makeActive) {
+    const currCoin = newCoins[currIndex];
+    if (
+      currCoin.wall !== CoinWall.Top &&
+      topCoin &&
+      topCoin.wall !== CoinWall.Bottom
+    ) {
+      totalChanges += Number(changeCoinState(topCoin, newCoins, 'active'));
+    }
+
+    if (
+      currCoin.wall !== CoinWall.Bottom &&
+      bottomCoin &&
+      bottomCoin.wall !== CoinWall.Top
+    ) {
+      totalChanges += Number(changeCoinState(bottomCoin, newCoins, 'active'));
+    }
+
+    if (
+      currCoin.wall !== CoinWall.Left &&
+      leftCoin &&
+      leftCoin.wall !== CoinWall.Right
+    ) {
+      totalChanges += Number(changeCoinState(leftCoin, newCoins, 'active'));
+    }
+
+    if (
+      currCoin.wall !== CoinWall.Right &&
+      rightCoin &&
+      rightCoin.wall !== CoinWall.Left
+    ) {
+      totalChanges += Number(changeCoinState(rightCoin, newCoins, 'active'));
+    }
+  } else {
+    totalChanges += Number(changeCoinState(topCoin, newCoins, 'none'));
+    totalChanges += Number(changeCoinState(bottomCoin, newCoins, 'none'));
+    totalChanges += Number(changeCoinState(leftCoin, newCoins, 'none'));
+    totalChanges += Number(changeCoinState(rightCoin, newCoins, 'none'));
+  }
+
+  return totalChanges;
+};
 
 export default function GameBoard({ game }: { game: Game }) {
   const [coins, setCoins] = useState<Coin[]>(() =>
@@ -22,102 +99,52 @@ export default function GameBoard({ game }: { game: Game }) {
   const [lastClickedIndex, setLastClickedIndex] = useState(-1);
   const [coinsCollected, setCoinsCollected] = useState(0);
   const [currScore, setCurrScore] = useState(0);
+  const gameColumns = game.cols;
 
-  const changeCoinState = (coin: Coin | null, state: CoinState) => {
-    if (coin && coin.state !== 'done') {
-      coin.state = state;
-    }
-  };
-
-  const handleNeighboringCoins = (
-    currIndex: number,
-    coins: Coin[],
-    makeActive: boolean
-  ) => {
-    const currCoin = coins[currIndex];
-    const topCoin =
-      currIndex - game.cols >= 0 ? coins[currIndex - game.cols] : null;
-    const bottomCoin =
-      currIndex + game.cols < game.coins.length
-        ? coins[currIndex + game.cols]
-        : null;
-    const leftCoin =
-      currIndex % game.cols && currIndex - 1 >= 0 ? coins[currIndex - 1] : null;
-    const rightCoin =
-      (currIndex + 1) % game.cols !== 0 && currIndex + 1 < game.coins.length
-        ? coins[currIndex + 1]
-        : null;
-
-    if (makeActive) {
-      currCoin.state = 'done';
-      currCoin.tabIndex = 0; // This is the new last interacted coin
-
-      if (
-        currCoin.wall !== CoinWall.Top &&
-        topCoin &&
-        topCoin.wall !== CoinWall.Bottom
-      ) {
-        changeCoinState(topCoin, 'active');
-      }
-
-      if (
-        currCoin.wall !== CoinWall.Bottom &&
-        bottomCoin &&
-        bottomCoin.wall !== CoinWall.Top
-      ) {
-        changeCoinState(bottomCoin, 'active');
-      }
-
-      if (
-        currCoin.wall !== CoinWall.Left &&
-        leftCoin &&
-        leftCoin.wall !== CoinWall.Right
-      ) {
-        changeCoinState(leftCoin, 'active');
-      }
-
-      if (
-        currCoin.wall !== CoinWall.Right &&
-        rightCoin &&
-        rightCoin.wall !== CoinWall.Left
-      ) {
-        changeCoinState(rightCoin, 'active');
-      }
-    } else {
-      changeCoinState(topCoin, 'none');
-      changeCoinState(bottomCoin, 'none');
-      changeCoinState(leftCoin, 'none');
-      changeCoinState(rightCoin, 'none');
-    }
-  };
-
+  console.log('inside the game board component');
   const handleCoinClick = (index: number, state: CoinState) => {
     console.log('coin clicked:', index, 'state:', state);
     if (state === 'active') {
       SoundManager.play('coin');
-      setCoins((prevCoins) => {
-        const newCoins = [...prevCoins];
 
-        // find other currently active coins, and make them inactive
-        if (lastClickedIndex !== -1) {
-          const lastClickedCoin = coins[lastClickedIndex];
-          lastClickedCoin.tabIndex = -1; // Now we have a new lat clicked coin, so set this one's tabIndex -1
-          handleNeighboringCoins(lastClickedIndex, newCoins, false);
+      // Create a new coins array
+      const newCoins = [...coins];
+
+      // Replace the old coin with a new object
+      const currCoin = coins[index];
+      newCoins[index] = { ...currCoin, state: 'done', tabIndex: 0 };
+
+      // find other currently active coins, and make them inactive
+      if (lastClickedIndex !== -1) {
+        // Now we have a new lat clicked coin, so set this one's tabIndex -1
+        newCoins[lastClickedIndex] = {
+          ...coins[lastClickedIndex],
+          tabIndex: -1,
+        };
+
+        handleNeighboringCoins(lastClickedIndex, newCoins, false, gameColumns);
+      }
+
+      if (!currCoin.isEnd) {
+        // find the coins that should be active now
+        const activeCoinsCount = handleNeighboringCoins(
+          index,
+          newCoins,
+          true,
+          gameColumns
+        );
+
+        if (!activeCoinsCount) {
+          console.log('No further moves');
         }
 
-        const currCoin = coins[index];
-        if (!currCoin.isEnd) {
-          // find the coins that should be active now
-          handleNeighboringCoins(index, coins, true);
+        // set this index as the last clicked index
+        setLastClickedIndex(index);
+      } else {
+        // The game is over
+      }
 
-          // set this index as the last clicked index
-          setLastClickedIndex(index);
-        } else {
-          // The game is over
-        }
-
-        return newCoins;
-      });
+      setCoins(newCoins);
     } else if (state === 'none') {
       SoundManager.play('deny');
     }
