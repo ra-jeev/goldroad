9e838a5db9119a825d666a096a22b83780b425e4
Rajeev R Sharma <i.rarsh@gmail.com>
2024-07-03 23:08:15 +0530

    feat: add about-btn component

---
+++ app/ui/about-btn.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { QuestionMarkCircleIcon } from '@heroicons/react/16/solid';
+import styles from '@/app/ui/about-btn.module.css';
+
+const LAST_UPDATE = 'game-update-300323';
+export default function AboutBtn() {
+  const [showDot, setShowDot] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const updateShown = localStorage.getItem(LAST_UPDATE) || 'no';
+    console.log(`inside the useEffect: updateShown: ${updateShown}`);
+    if (updateShown === 'no') {
+      setShowDot(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (pathname === '/about') {
+      console.log('the pathname is about....hiding the dot');
+      setShowDot(false);
+      localStorage.setItem(LAST_UPDATE, 'yes');
+    }
+  }, [pathname]);
+
+  return (
+    <Link href='/about' className='icon-btn icon-wrapper'>
+      <QuestionMarkCircleIcon />
+      {showDot && <span className={styles['notification-dot']} />}
+    </Link>
+  );
+}
