0fc2b32d71ba535d3866a1e7be5039972ece8f6a
Rajeev R Sharma <i.rarsh@gmail.com>
2024-07-03 23:11:51 +0530

    feat: add AppHeader to root layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,5 @@ import styles from './page.module.css';
 import GameContainer from '@/app/ui/game/game-container';
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <GameContainer />
-    </main>
-  );
+  return <GameContainer />;
 }
