09772c6c1fcc792f5f61014a4a573d52ffaac9fb
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-03 16:43:29 +0530

    feat: create utils functions

---
+++ app/lib/utils.ts
@@ -0,0 +1,26 @@
+export const areDatesEqual = (
+  date1: Date | string | number,
+  date2: Date | string | number,
+  checkUTC = false
+): boolean => {
+  const d1 = date1 instanceof Date ? date1 : new Date(date1);
+  const d2 = date2 instanceof Date ? date2 : new Date(date2);
+
+  if (isNaN(d1.getTime()) || isNaN(d2.getTime())) {
+    throw new Error('Invalid date provided');
+  }
+
+  return checkUTC
+    ? d1.getUTCFullYear() === d2.getUTCFullYear() &&
+        d1.getUTCMonth() === d2.getUTCMonth() &&
+        d1.getUTCDate() === d2.getUTCDate()
+    : d1.getFullYear() === d2.getFullYear() &&
+        d1.getMonth() === d2.getMonth() &&
+        d1.getDate() === d2.getDate();
+};
+
+export const getOrdinalSuffix = (n: number): string => {
+  const suffix = ['th', 'st', 'nd', 'rd'];
+  const v = n % 100;
+  return suffix[(v - 20) % 10] || suffix[v] || suffix[0];
+};
