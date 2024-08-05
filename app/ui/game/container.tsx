3a8175f0aa74d7bea07b7235bb8cc438887cec2d
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-05 17:13:35 +0530

    feat: update game container

--- app/ui/game/container.tsx
+++ app/ui/game/container.tsx
@@ -1,8 +1,9 @@
 import { fetchGame } from '@/app/lib/data';
 import GameBoard from '@/app/ui/game/board';
+import GameNotFound from '@/app/ui/game/not-found';
 
 export default async function GameContainer() {
   const game = await fetchGame();
 
-  return <GameBoard game={game} />;
+  return game ? <GameBoard game={game} /> : <GameNotFound />;
 }
