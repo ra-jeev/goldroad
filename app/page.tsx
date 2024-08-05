1f58aa560025703f4cb653b949702bf805990616
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-05 23:13:31 +0530

    feat: make home page force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import styles from './page.module.css';
 import GameContainer from '@/app/ui/game/container';
+
+export const dynamic = 'force-dynamic';
 
 export default function Home() {
   return <GameContainer />;
