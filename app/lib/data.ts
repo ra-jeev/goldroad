55c4994f7766ad7e8de98f299f3f1f759d630991
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-05 17:13:03 +0530

    feat: add game fetch from DB

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,12 +1,23 @@
-import { gameData } from '@/app/lib/dummy-data';
+import { ObjectId } from 'mongodb';
+import mongoClientPromise from '@/app/lib/mongodb';
 import { Game } from '@/app/lib/types';
 
-export async function fetchGame() {
-  const game = await new Promise<Game>((resolve) => {
-    setTimeout(() => {
-      resolve(gameData);
-    }, 2500);
-  });
+type DbGame = Omit<Game, '_id'> & { _id: ObjectId };
 
-  return game;
+export async function fetchGame(gameNo?: number): Promise<Game | null> {
+  const client = await mongoClientPromise;
+
+  const db = client.db(process.env.DB_NAME);
+  const filter = gameNo
+    ? {
+        active: true,
+        gameNo,
+      }
+    : { current: true };
+
+  const dbGame = await db.collection<DbGame>('games').findOne(filter);
+
+  console.log('fetched db game', dbGame);
+
+  return dbGame ? { ...dbGame, _id: dbGame._id.toString() } : null;
 }
