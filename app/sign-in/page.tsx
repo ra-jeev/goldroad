75b488d0d57bfe43d5385a693d5c8e6a0d8b4d6d
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-09 23:12:08 +0530

    feat: basic sign in page with Google Sign in

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,3 +1,18 @@
+import GoogleSignInBtn from '@/app/ui/google-sign-in-btn';
+
 export default function Page() {
-  return <p>SignIn Page</p>;
+  return (
+    <div className='info-container'>
+      <h1 className='info-title'>Sign in to your account</h1>
+      <p className='font-medium mt-2'>
+        Take your playing history with you, wherever you go...
+      </p>
+
+      <GoogleSignInBtn className='mt-12' />
+
+      {/* {showError && signingStatus && (
+        <p className='sign-in-error'>{signingStatus}</p>
+      )} */}
+    </div>
+  );
 }
