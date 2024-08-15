1fcbdd20059e778901d0db12266cd3a8eb1b96c2
Rajeev R Sharma <i.rarsh@gmail.com>
2024-08-15 18:22:10 +0530

    feat: update PersonalStats component with correct input data

--- app/ui/stats/personal.tsx
+++ app/ui/stats/personal.tsx
@@ -1,20 +1,68 @@
 import clsx from 'clsx';
+import { formatQuantity } from '@/app/lib/utils';
 import styles from '@/app/ui/stats/personal.module.css';
 
-type StatItem = {
-  key: string;
-  value: string;
+type StatsData = {
+  currStreak: number;
+  isCurrLongestStreak: boolean;
+  longestStreak: number;
+  played: number;
+  solves: number;
+  solveStats: { [key: string]: number };
 };
 
 export default function PersonalStats({
-  overallStats,
+  playerStats,
 }: {
-  overallStats: StatItem[];
+  playerStats: StatsData;
 }) {
+  const formatPersonalStats = (statsData: StatsData) => {
+    const completionRate = (statsData.solves / statsData.played) * 100;
+
+    let totalTries = 0;
+    let totalSolves = 0;
+    Object.entries(statsData.solveStats).forEach(([tries, count]) => {
+      totalTries += Number(tries) * count;
+      totalSolves += count;
+    });
+
+    return [
+      {
+        key: 'Current Streak',
+        value: formatQuantity(statsData.currStreak, 'day'),
+      },
+      {
+        key: 'Longest Streak',
+        value: statsData.isCurrLongestStreak
+          ? 'This ☝️ one'
+          : formatQuantity(statsData.longestStreak, 'day'),
+      },
+      {
+        key: 'Total Treads',
+        value: formatQuantity(statsData.played, 'road'),
+      },
+      {
+        key: 'Total Finishes',
+        value: formatQuantity(statsData.solves, 'road'),
+      },
+      {
+        key: 'Completion Rate',
+        value: `${completionRate.toFixed(0)}%`,
+      },
+      {
+        key: 'Average Tries',
+        value: formatQuantity(totalTries / totalSolves, 'try', 1),
+      },
+    ];
+  };
+
+  const personalStats = formatPersonalStats(playerStats);
+
   return (
     <div className={clsx('card', 'stats-card')}>
       <div className='card-title'>Your Stats</div>
-      {overallStats.map((statItem, index) => {
+
+      {personalStats.map((statItem, index) => {
         return (
           <div key={`personal-stats-${index}`} className={styles.statsEntry}>
             <div className={styles.statsKey}>{statItem.key}:</div>
