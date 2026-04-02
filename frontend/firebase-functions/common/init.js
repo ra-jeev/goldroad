3ddf323ea2253e1a8bffb93efbb11d07fb3349db
Rajeev R Sharma <i.rarsh@gmail.com>
2026-04-02 15:17:43 +0530

    chore: remove legacy code

--- frontend/firebase-functions/common/init.js
+++
@@ -1,7 +0,0 @@
-const { getApp, getApps, initializeApp } = require('firebase-admin/app');
-const { initializeFirestore } = require('firebase-admin/firestore');
-
-const app = !getApps().length ? initializeApp() : getApp();
-const fireStore = initializeFirestore(app, { preferRest: true });
-
-module.exports = { fireStore };
