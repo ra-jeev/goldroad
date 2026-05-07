030883e2c105fd90ce767ae0364e0b5f42bb0ec2
Rajeev R Sharma <i.rarsh@gmail.com>
2026-05-07 21:12:11 +0530

    chore: add vueuse dep

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,13 +6,13 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   nitro: {
-    preset: "cloudflare_module",
+    preset: 'cloudflare_module',
 
     cloudflare: {
       deployConfig: true,
-      nodeCompat: true
-    }
+      nodeCompat: true,
+    },
   },
 
-  modules: ["nitro-cloudflare-dev"]
-})
+  modules: ['nitro-cloudflare-dev', '@vueuse/nuxt'],
+});
